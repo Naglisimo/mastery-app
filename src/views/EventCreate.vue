92c6c4753632cb01d0c8760166c84ace85d01955
Naglisimo <naglisk@gmail.com>
2022-07-11 22:05:50 +0300

    src files

---
+++ src/views/EventCreate.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>Event create</div>
+</template>
---
+++ src/views/EventList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div>Event list</div>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+<script>
+import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService'
+
+export default {
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      events: []
+    }
+  },
+  created() {
+    EventService.getEvents()
+      .then(({ data }) => {
+        console.log(data)
+        this.events = data
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+}
+</script>
